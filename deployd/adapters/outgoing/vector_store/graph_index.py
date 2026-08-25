6a70f76e55fc9e85db08c8390060ad55ed7e5d01
bmm333 <benmema3@gmail.com>
2026-08-25 23:23:46 +0200

    [DID-7] feat: Graph Index

--- deployd/adapters/outgoing/vector_store/graph_index.py
+++ deployd/adapters/outgoing/vector_store/graph_index.py
@@ -24,10 +24,10 @@ class GraphIndex:
         hits = [
             StructuralHit(
                 runbook_id=structure.runbook_id,
-                causal_score=self._causal_chain_similarity(  # type: ignore[attr-defined]
+                causal_score=self._causal_chain_similarity(
                     current_causal_chain, structure.causal_chain
                 ),
-                component_score=self._component_overlap(  # type: ignore[attr-defined]
+                component_score=self._component_overlap(
                     current_components, structure.affected_components
                 ),
             )
@@ -36,5 +36,29 @@ class GraphIndex:
         hits.sort(key=lambda h: h.causal_score, reverse=True)
         return hits[:top_k]
 
+    @staticmethod
+    def _causal_chain_similarity(chain_a: tuple[str, ...], chain_b: tuple[str, ...]) -> float:
+        """LSC ratio, length of the longer chains. same event on the same order is strong signal"""
+        if not chain_a or not chain_b:
+            return 0.0
+        lcs_length = _longest_common_subsequence_length(chain_a, chain_b)
+        return lcs_length / max(len(chain_a), len(chain_b))
 
-# fuck this
+    @staticmethod
+    def _component_overlap(components_a: frozenset[str], components_b: frozenset[str]) -> float:
+        """Jaccard"""
+        union = components_a | components_b
+        if not union:
+            return 0.0
+        return len(components_a & components_b) / len(union)
+
+
+def _longest_common_subsequence_length(a: tuple[str, ...], b: tuple[str, ...]) -> int:
+    dp = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            if a[i - 1] == b[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[len(a)][len(b)]
